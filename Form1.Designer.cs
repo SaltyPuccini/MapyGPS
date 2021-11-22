05cb73b56d50505a5699a5290e7011a742254911
SaltyPuccini <kkajetanwojciechowski@gmail.com>
2021-11-23 00:03:14 +0100

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace MapyGPS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.gMapMap = new GMap.NET.WindowsForms.GMapControl();
+            this.buttonShowOnMap = new System.Windows.Forms.Button();
+            this.textBoxLong = new System.Windows.Forms.TextBox();
+            this.textBoxLati = new System.Windows.Forms.TextBox();
+            this.labelLong = new System.Windows.Forms.Label();
+            this.labelLati = new System.Windows.Forms.Label();
+            this.buttonComPort = new System.Windows.Forms.Button();
+            this.NMEAlabel = new System.Windows.Forms.Label();
+            this.comboBoxCOMPORT = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.buttonBTDevices = new System.Windows.Forms.Button();
+            this.listBoxBT = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(569, 518);
+            this.splitter1.TabIndex = 0;
+            this.splitter1.TabStop = false;
+            // 
+            // gMapMap
+            // 
+            this.gMapMap.Bearing = 0F;
+            this.gMapMap.CanDragMap = true;
+            this.gMapMap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapMap.GrayScaleMode = false;
+            this.gMapMap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapMap.LevelsKeepInMemory = 5;
+            this.gMapMap.Location = new System.Drawing.Point(12, 12);
+            this.gMapMap.MarkersEnabled = true;
+            this.gMapMap.MaxZoom = 2;
+            this.gMapMap.MinZoom = 2;
+            this.gMapMap.MouseWheelZoomEnabled = true;
+            this.gMapMap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapMap.Name = "gMapMap";
+            this.gMapMap.NegativeMode = false;
+            this.gMapMap.PolygonsEnabled = true;
+            this.gMapMap.RetryLoadTile = 0;
+            this.gMapMap.RoutesEnabled = true;
+            this.gMapMap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapMap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapMap.ShowTileGridLines = false;
+            this.gMapMap.Size = new System.Drawing.Size(540, 494);
+            this.gMapMap.TabIndex = 1;
+            this.gMapMap.Zoom = 0D;
+            // 
+            // buttonShowOnMap
+            // 
+            this.buttonShowOnMap.Location = new System.Drawing.Point(575, 68);
+            this.buttonShowOnMap.Name = "buttonShowOnMap";
+            this.buttonShowOnMap.Size = new System.Drawing.Size(208, 35);
+            this.buttonShowOnMap.TabIndex = 2;
+            this.buttonShowOnMap.Text = "show location on map";
+            this.buttonShowOnMap.UseVisualStyleBackColor = true;
+            this.buttonShowOnMap.Click += new System.EventHandler(this.buttonShowOnMap_Click);
+            // 
+            // textBoxLong
+            // 
+            this.textBoxLong.Location = new System.Drawing.Point(647, 38);
+            this.textBoxLong.Name = "textBoxLong";
+            this.textBoxLong.Size = new System.Drawing.Size(125, 22);
+            this.textBoxLong.TabIndex = 3;
+            this.textBoxLong.TextChanged += new System.EventHandler(this.textBoxLong_TextChanged);
+            // 
+            // textBoxLati
+            // 
+            this.textBoxLati.Location = new System.Drawing.Point(647, 14);
+            this.textBoxLati.Name = "textBoxLati";
+            this.textBoxLati.Size = new System.Drawing.Size(125, 22);
+            this.textBoxLati.TabIndex = 4;
+            // 
+            // labelLong
+            // 
+            this.labelLong.AutoSize = true;
+            this.labelLong.Location = new System.Drawing.Point(572, 38);
+            this.labelLong.Name = "labelLong";
+            this.labelLong.Size = new System.Drawing.Size(66, 17);
+            this.labelLong.TabIndex = 5;
+            this.labelLong.Text = "longitude";
+            // 
+            // labelLati
+            // 
+            this.labelLati.AutoSize = true;
+            this.labelLati.Location = new System.Drawing.Point(572, 14);
+            this.labelLati.Name = "labelLati";
+            this.labelLati.Size = new System.Drawing.Size(54, 17);
+            this.labelLati.TabIndex = 6;
+            this.labelLati.Text = "latidute";
+            // 
+            // buttonComPort
+            // 
+            this.buttonComPort.Location = new System.Drawing.Point(575, 440);
+            this.buttonComPort.Name = "buttonComPort";
+            this.buttonComPort.Size = new System.Drawing.Size(208, 36);
+            this.buttonComPort.TabIndex = 8;
+            this.buttonComPort.Text = "beginCom";
+            this.buttonComPort.UseVisualStyleBackColor = true;
+            this.buttonComPort.Click += new System.EventHandler(this.buttonOpenSerial);
+            // 
+            // NMEAlabel
+            // 
+            this.NMEAlabel.AutoSize = true;
+            this.NMEAlabel.Location = new System.Drawing.Point(589, 420);
+            this.NMEAlabel.Name = "NMEAlabel";
+            this.NMEAlabel.Size = new System.Drawing.Size(183, 17);
+            this.NMEAlabel.TabIndex = 9;
+            this.NMEAlabel.Text = "NMEA STRING UNAVIABLE";
+            // 
+            // comboBoxCOMPORT
+            // 
+            this.comboBoxCOMPORT.FormattingEnabled = true;
+            this.comboBoxCOMPORT.Location = new System.Drawing.Point(575, 482);
+            this.comboBoxCOMPORT.Name = "comboBoxCOMPORT";
+            this.comboBoxCOMPORT.Size = new System.Drawing.Size(208, 24);
+            this.comboBoxCOMPORT.TabIndex = 10;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // buttonBTDevices
+            // 
+            this.buttonBTDevices.Location = new System.Drawing.Point(579, 282);
+            this.buttonBTDevices.Name = "buttonBTDevices";
+            this.buttonBTDevices.Size = new System.Drawing.Size(204, 39);
+            this.buttonBTDevices.TabIndex = 11;
+            this.buttonBTDevices.Text = "BTDevices";
+            this.buttonBTDevices.UseVisualStyleBackColor = true;
+            this.buttonBTDevices.Click += new System.EventHandler(this.buttonBTDevices_Click);
+            // 
+            // listBoxBT
+            // 
+            this.listBoxBT.FormattingEnabled = true;
+            this.listBoxBT.ItemHeight = 16;
+            this.listBoxBT.Location = new System.Drawing.Point(579, 161);
+            this.listBoxBT.Name = "listBoxBT";
+            this.listBoxBT.Size = new System.Drawing.Size(203, 100);
+            this.listBoxBT.TabIndex = 12;
+            this.listBoxBT.SelectedIndexChanged += new System.EventHandler(this.listBoxBT_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 518);
+            this.Controls.Add(this.listBoxBT);
+            this.Controls.Add(this.buttonBTDevices);
+            this.Controls.Add(this.comboBoxCOMPORT);
+            this.Controls.Add(this.NMEAlabel);
+            this.Controls.Add(this.buttonComPort);
+            this.Controls.Add(this.labelLati);
+            this.Controls.Add(this.labelLong);
+            this.Controls.Add(this.textBoxLati);
+            this.Controls.Add(this.textBoxLong);
+            this.Controls.Add(this.buttonShowOnMap);
+            this.Controls.Add(this.gMapMap);
+            this.Controls.Add(this.splitter1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Splitter splitter1;
+        private GMap.NET.WindowsForms.GMapControl gMapMap;
+        private System.Windows.Forms.Button buttonShowOnMap;
+        private System.Windows.Forms.TextBox textBoxLong;
+        private System.Windows.Forms.TextBox textBoxLati;
+        private System.Windows.Forms.Label labelLong;
+        private System.Windows.Forms.Label labelLati;
+        private System.Windows.Forms.Button buttonComPort;
+        private System.Windows.Forms.Label NMEAlabel;
+        private System.Windows.Forms.ComboBox comboBoxCOMPORT;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button buttonBTDevices;
+        private System.Windows.Forms.ListBox listBoxBT;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,265 @@
+﻿using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+using Ghostware.NMEAParser.Enums;
+using System.IO.Ports;
+using Ghostware.NMEAParser.NMEAMessages;
+
+namespace MapyGPS
+{
+
+    public partial class Form1 : Form
+    {
+
+        bool nadawanie = true;
+        double latidute = 0;
+        double longtidute = 0;
+
+        static BluetoothClient bluetoothClient = new BluetoothClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+            gMapMap.ShowCenter = false;
+
+
+        }
+        
+        BluetoothDeviceInfo[] devices;
+
+        private void buttonShowOnMap_Click(object sender, EventArgs e)
+        {
+            gMapMap.DragButton = MouseButtons.Left;
+            gMapMap.MapProvider = GMapProviders.GoogleMap;
+
+            if (!string.IsNullOrWhiteSpace(textBoxLong.Text))
+                latidute = Convert.ToDouble(textBoxLati.Text);
+            if (!string.IsNullOrWhiteSpace(textBoxLong.Text))
+                longtidute = Convert.ToDouble(textBoxLong.Text);
+
+            gMapMap.Position = new GMap.NET.PointLatLng(latidute, longtidute);
+            gMapMap.MinZoom = 0;
+            gMapMap.MaxZoom = 18;
+            gMapMap.Zoom = 10;
+
+            PointLatLng point = new PointLatLng(latidute, longtidute);
+            GMapMarker marker;
+            marker = new GMarkerGoogle(point, GMarkerGoogleType.red_dot);
+            GMapOverlay markers = new GMapOverlay("markers");
+            markers.Markers.Add(marker);
+            gMapMap.Overlays.Add(markers);
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //gdyby jednak trzbea pracować na comportach
+            string[] ports = SerialPort.GetPortNames();
+            comboBoxCOMPORT.Items.AddRange(ports);
+
+            devices = bluetoothClient.DiscoverDevices();
+            for (int i = 0; i < devices.Length; i++)
+            {
+                listBoxBT.Items.Add(devices[i].DeviceName);
+            }
+        }
+
+
+        //część dotycząca pracy na serial portach
+
+        string dataIN;
+
+        //otwiera port
+        private void buttonOpenSerial(object sender, EventArgs e)
+        {
+            //https://en.wikipedia.org/wiki/NMEA_0183
+
+            try
+            {
+                serialPort1.PortName = comboBoxCOMPORT.Text;
+                serialPort1.BaudRate = 4800;
+                serialPort1.DataBits = 8;
+                serialPort1.StopBits = StopBits.One;
+                serialPort1.Parity = Parity.None;
+
+                serialPort1.Open();               
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }            
+
+        //sprawdza co dostał
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            dataIN = serialPort1.ReadExisting();
+            this.Invoke(new EventHandler(ShowData));
+        }
+        //wypisuje co dostał na label
+        private void ShowData(object sender, EventArgs e)
+        {
+            NMEAlabel.Text = dataIN;
+            
+        }
+
+
+        //część dotycząca BT
+
+
+        //paruje z wybranym urządzeniem - docelowo jest zrobione tak, że paruje się to z GPSEM i potem od razu wywołuje funkcje do parsowania NMEA na lati i long
+        private void buttonBTDevices_Click(object sender, EventArgs e)
+        {
+            Paruj();
+        }
+
+
+        public void Paruj()
+        {
+            BluetoothDeviceInfo deviceToPair;
+            var devices = bluetoothClient.DiscoverDevices();
+            deviceToPair = devices.Where(x => x.DeviceName.Equals(listBoxBT.SelectedItem)).FirstOrDefault();
+            if (deviceToPair != null)
+            {
+
+                //136 - 145 opcjonalne
+                var blueToothInfo = string.Format(                      
+                    "- DeviceName: {0}{1}  Connected: {2}{1}  Address: {3}{1}  Last seen: {4}{1}  Last used: {5}{1}", deviceToPair.DeviceName, Environment.NewLine, deviceToPair.Connected, deviceToPair.DeviceAddress, deviceToPair.LastSeen, deviceToPair.LastUsed);
+
+                blueToothInfo += string.Format(
+                    "  Class of device{0}   Device: {1}{0}   Major Device: {2}{0}   Service: {3}", Environment.NewLine, deviceToPair.ClassOfDevice.Device, deviceToPair.ClassOfDevice.MajorDevice, deviceToPair.ClassOfDevice.Service);
+                
+                
+                
+                MessageBox.Show(string.Format("Urządzenie z którym się parujemy:\n{0}", blueToothInfo));
+                
+                deviceToPair.Update();
+                deviceToPair.Refresh();
+                deviceToPair.SetServiceState(BluetoothService.ObexObjectPush, true);
+                
+                bool isPaired = BluetoothSecurity.PairRequest(deviceToPair.DeviceAddress, "0000");
+
+                if (isPaired)
+                {
+                    MessageBox.Show("Urządzenie sparowane");
+                    bluetoothClient.BeginConnect(deviceToPair.DeviceAddress, BluetoothService.SerialPort, new AsyncCallback(Connect), deviceToPair);
+                }
+                else
+                {
+                    MessageBox.Show("Nie udało się sparować urządzenia.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("device to Par = null");
+            }
+        }
+
+        private void Dummy(IAsyncResult ar)
+        {
+            Console.WriteLine("połączone i nic więcej");
+        }
+
+        //tłumaczy NMEA na dane i wpisuje do textboxów lati i long.
+        private void Connect(IAsyncResult result)
+        {
+
+            if (result.IsCompleted)
+            {
+                var myNetworkStream = bluetoothClient.GetStream();
+                while (true)
+                {
+                    if (myNetworkStream.CanRead)
+                    {
+                        byte[] myReadBuffer = new byte[256];
+                        StringBuilder myCompleteMessage = new StringBuilder();
+                        int numberOfBytesRead = 0;
+
+                        do
+                        {
+                            numberOfBytesRead = myNetworkStream.Read(myReadBuffer, 0, myReadBuffer.Length);
+
+                            myCompleteMessage.AppendFormat("{0}", Encoding.ASCII.GetString(myReadBuffer, 0, numberOfBytesRead));
+
+                        }
+                        while (myNetworkStream.DataAvailable);
+
+                        try
+                        {
+                            Ghostware.NMEAParser.NmeaParser parser = new Ghostware.NMEAParser.NmeaParser();
+                            GpggaMessage parsedMessage = (GpggaMessage)parser.Parse(myCompleteMessage.ToString());
+                            Console.WriteLine("You received the following message : " + myCompleteMessage.ToString());
+
+                            nadawanie = true;
+
+                            textBoxLati.Text = parsedMessage.Latitude.ToString();
+                            textBoxLong.Text = parsedMessage.Longitude.ToString();
+
+                        }
+                        catch (Exception exc)
+                        {
+                            Console.WriteLine("Wystapil blad, ponowna proba pobrania danych");
+                            Console.WriteLine();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry.  You cannot read from this NetworkStream.");
+                    }
+                }
+            }
+        }
+
+        private void listBoxBT_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine(listBoxBT.SelectedItem);
+        }
+
+        //jeśli zaszły zmiany w textboxach, pojawia się pin na mapie. Może nie zadziałać i się sfreezować, tu coś z wątkami możliwe trzeba będzie porobić, albo ograniczyć się do np. nasłuchiwania tylko jednego komunikatu z BT na przycisk zamiast w pętli.
+
+        private void textBoxLong_TextChanged(object sender, EventArgs e)// ???
+        {
+
+            gMapMap.DragButton = MouseButtons.Left;
+            gMapMap.MapProvider = GMapProviders.GoogleMap;
+
+            if (nadawanie)
+            {
+                if (!string.IsNullOrWhiteSpace(textBoxLong.Text))
+                    latidute = Convert.ToDouble(textBoxLati.Text);
+                if (!string.IsNullOrWhiteSpace(textBoxLong.Text))
+                    longtidute = Convert.ToDouble(textBoxLong.Text);
+
+                gMapMap.Position = new GMap.NET.PointLatLng(latidute, longtidute);
+                gMapMap.MinZoom = 0;
+                gMapMap.MaxZoom = 18;
+                gMapMap.Zoom = 10;
+
+                PointLatLng point = new PointLatLng(latidute, longtidute);
+                GMapMarker marker;
+                marker = new GMarkerGoogle(point, GMarkerGoogleType.red_dot);
+                GMapOverlay markers = new GMapOverlay("markers");
+                markers.Markers.Add(marker);
+                gMapMap.Overlays.Add(markers);
+                //nadawanie = false;
+            }
+        }
+    }
+}
